ff9fe6fce2e2d48c32a91f5473a6d61bff3b2917
tanitta <tanitta@trit33.local>
2017-12-01 14:25:24 +0900

    Enable to move player with cursor keys.

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+	public float speed = 0.1f;
+
+	void Start () {
+		
+	}
+
+	void Update () {
+		float x = Input.GetAxisRaw ("Horizontal");
+
+		float y = Input.GetAxisRaw ("Vertical");
+
+		Vector3 direction = new Vector3 (x, y, 0).normalized;
+
+		GetComponent<Transform> ().position = GetComponent<Transform> ().position + direction * speed;
+	}
+}
