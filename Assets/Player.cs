e79b3a5c7bd59932c6aa0123e12c1794421e4405
tanitta <tanitta@trit33.local>
2017-12-01 16:26:30 +0900

    Add enemy.

---
+++ Assets/Player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+	public GameObject bullet;
+
+	void Update () {
+		float x = Input.GetAxisRaw ("Horizontal");
+
+		float y = Input.GetAxisRaw ("Vertical");
+
+		Vector3 direction = new Vector3 (x, y, 0).normalized;
+
+		GetComponent<Monkey>().Move (direction);
+	}
+		
+	IEnumerator Start ()
+	{
+		while (true) {
+			GetComponent<Monkey> ().Shot (transform, bullet);
+			yield return new WaitForSeconds (0.5f);
+		}
+	}
+}
